d4542a07d973d10410e40c31f12d20a77f2cd3d0
Thantzinwin <thant.zin.winnnn@gmail.com>
2022-09-03 21:03:43 +0630

    React

---
+++ src/Components/ExpensiveItems.js
@@ -0,0 +1,12 @@
+function ExpensiveItems(){
+    return (<div>
+            <h1>Expensive  Items!</h1>
+            <p>Hello World</p>
+    </div>
+            
+);
+
+}
+
+
+export default ExpensiveItems;
