19c04b10b2cb690980d6bb85773fa86eb5df7926
Thantzinwin <thant.zin.winnnn@gmail.com>
2022-09-04 23:50:36 +0630

    react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
-import ExpensiveItems from './Components/ExpensiveItems';
+import Counter from './Components/Counter';
+// import ExpensiveItems from './Components/ExpensiveItems';
+
 
 function App() {
+
   return (
     <div>
-      <h1>Hello World</h1>
-      <ExpensiveItems></ExpensiveItems>
+      {/* <h1>Hello World</h1>
+      <ExpensiveItems></ExpensiveItems> */}
+
+      <Counter initalValue={4} incValue={2}></Counter>
     </div>
   );
 }
---
+++ src/Components/Counter.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default class Counter extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            counter : this.props.initalValue
+        }
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this)
+    }
+    componentDidMount(){
+        console.log('componentDidMount')
+    }
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
+    }
+    increment()
+    {
+        console.log('Inc');
+        // this.setState({
+        //     counter : this.state.counter + this.props.incValue
+        // })
+
+        this.setState((state,props)=>({
+            counter : state.counter + props.incValue
+        }))
+    }
+    decrement()
+    {
+        console.log('Dec');
+        // this.setState({
+        //     counter : this.state.counter - this.props.incValue
+        // })
+
+        this.setState((state,props)=>({
+            counter : state.counter - props.incValue
+        }))
+    }
+
+    render(){
+        console.log('render');
+        return(
+            <div>
+                Counter
+                <button type="button" onClick={this.increment}>+</button>
+                &nbsp;{this.state.counter}&nbsp;
+                <button type="button" onClick={this.decrement}>-</button>
+            </div>
+        )
+    }
+}
