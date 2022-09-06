f242b791bba4fd7b7e9d06d92822fc84e1059a5f
Thantzinwin <thant.zin.winnnn@gmail.com>
2022-09-06 15:57:38 +0630

    React components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 
 import './App.css';
-import Counter from './Components/Counter';
-// import ExpensiveItems from './Components/ExpensiveItems';
-
+import Expense from "./Components/Expenses/Expense";
 
 function App() {
-
+  const expenses = [
+    { 
+      id: 'e1',
+      title: "Toliet Paper", 
+      amount: 94.12, 
+      date: new Date(2020, 7, 14) 
+    },
+    { 
+      id: 'e2',
+      title: "New TV", 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
+    { 
+      id: 'e3',
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28) 
+    },
+    { 
+      id: 'e4',
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12) 
+    },
+  ];
   return (
-    <div>
-      {/* <h1>Hello World</h1>
-      <ExpensiveItems></ExpensiveItems> */}
-
-      <Counter initalValue={4} incValue={2}></Counter>
-    </div>
+    <Expense expenses={expenses}></Expense>
   );
 }
 
